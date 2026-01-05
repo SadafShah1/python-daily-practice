6b6f1057954de40ed74d2c799a6ac2cc3db1d124
Sadaf Bibi <sadaf.bibi.dev@gmail.com>
2026-01-05 10:53:16 +0500

    Day 9: DataBase with SQlite

---
+++ Day_9.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import sqlite3
+
+conn = sqlite3.connect("users.db")
+cursor = conn.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    email TEXT
+)
+""")
+
+conn.commit()
+conn.close()
+
+print("Table created successfully")
+
+
+# In[ ]:
+
+
+
+
