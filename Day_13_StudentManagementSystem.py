3d8344819c7a579f572ecda8a4cc4a9605ea6d6d
Sadaf Bibi <sadaf.bibi.dev@gmail.com>
2026-01-09 10:22:44 +0500

    Implement student management menu
    Added a menu for student management options.

--- Day_13_StudentManagementSystem.py
+++ Day_13_StudentManagementSystem.py
@@ -48,4 +48,25 @@ def delete_student():
             return
 
     print("Student not found\n")
+while True:
+    print("1. Add Student")
+    print("2. View Students")
+    print("3. Search Student")
+    print("4. Delete Student")
+    print("5. Exit")
 
+    choice = input("Choose option: ")
+
+    if choice == "1":
+        add_student()
+    elif choice == "2":
+        view_students()
+    elif choice == "3":
+        search_student()
+    elif choice == "4":
+        delete_student()
+    elif choice == "5":
+        print("Goodbye!")
+        break
+    else:
+        print("Invalid choice\n")
