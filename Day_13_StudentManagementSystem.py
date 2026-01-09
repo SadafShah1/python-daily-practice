f1adb0452762cad4b67fda11c567eb1aa115585b
Sadaf Bibi <sadaf.bibi.dev@gmail.com>
2026-01-09 10:20:33 +0500

    Add functions to manage student records

--- Day_13_StudentManagementSystem.py
+++ Day_13_StudentManagementSystem.py
@@ -1,1 +1,26 @@
+students = []
+
+
+def add_student():
+    name = input("Enter student name: ")
+    roll = input("Enter roll number: ")
+    marks = input("Enter marks: ")
+
+    student = {
+        "name": name,
+        "roll": roll,
+        "marks": marks
+    }
+students.append(student)
+    print("Student added successfully\n")
+
+
+def view_students():
+    if not students:
+        print("No students found\n")
+        return
+
+    for s in students:
+        print(f"Name: {s['name']}, Roll: {s['roll']}, Marks: {s['marks']}")
+    print()
 
