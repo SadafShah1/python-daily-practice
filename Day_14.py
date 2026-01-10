a602eac83d469d86604db495dc2a6cf2308b7d69
Sadaf Bibi <sadaf.bibi.dev@gmail.com>
2026-01-10 23:30:07 +0500

    Update Day_14.py

--- Day_14.py
+++ Day_14.py
@@ -1,1 +1,17 @@
+expenses = []
+
+
+def add_expense():
+    title = input("Enter expense title: ")
+    amount = float(input("Enter amount: "))
+    category = input("Enter category: ")
+
+    expense = {
+        "title": title,
+        "amount": amount,
+        "category": category
+    }
+
+    expenses.append(expense)
+    print("Expense added successfully\n")
 
