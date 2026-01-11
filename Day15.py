0f0e5de45dedaf532f1c52c0deb4c755906857eb
Sadaf Bibi <sadaf.bibi.dev@gmail.com>
2026-01-11 22:38:49 +0500

    Add book loading and saving functions

--- Day15.py
+++ Day15.py
@@ -1,1 +1,32 @@
+import os
 
+FILE_NAME = "books.txt"
+
+
+def load_books():
+    books = []
+    if os.path.exists(FILE_NAME):
+        with open(FILE_NAME, "r") as f:
+            for line in f:
+                title, author, status = line.strip().split("|")
+                books.append({
+                    "title": title,
+                    "author": author,
+                    "status": status
+                })
+    return books
+  def save_books(books):
+    with open(FILE_NAME, "w") as f:
+        for b in books:
+            f.write(f"{b['title']}|{b['author']}|{b['status']}\n")
+
+
+def add_book(books):
+    title = input("Book title: ")
+    author = input("Author: ")
+
+    books.append({
+        "title": title,
+        "author": author,
+        "status": "available"
+    })
