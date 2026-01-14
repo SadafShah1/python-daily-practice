112b78519707107e2051fbfc617fff301a803e90
Sadaf Bibi <sadaf.bibi.dev@gmail.com>
2026-01-14 23:47:53 +0500

    Add display and is_pass methods to class

--- Day17.py
+++ Day17.py
@@ -4,4 +4,11 @@ class Student:
         self.roll = roll
         self.marks = marks
 
-    
+    def display(self):
+        print(f"Name: {self.name}")
+        print(f"Roll No: {self.roll}")
+        print(f"Marks: {self.marks}")
+
+    def is_pass(self):
+        return self.marks >= 40
+
