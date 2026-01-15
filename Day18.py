bb1ff8f15dd169ba08f5b540cfb8fd69f9158ca5
Sadaf Bibi <sadaf.bibi.dev@gmail.com>
2026-01-15 22:24:22 +0500

    Add User and Admin classes with basic methods

--- Day18.py
+++ Day18.py
@@ -1,1 +1,15 @@
+class User:
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
 
+    def login(self):
+        print(f"{self.username} logged in")
+
+class Admin(User):
+    def __init__(self, username, email, role):
+        super().__init__(username, email)
+        self.role = role
+
+    def delete_user(self, user):
+        print(f"{self.username} deleted user {user}")
