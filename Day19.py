2bfc1b57a1911a38c06c090fe93152f4b1ff8153
Sadaf Bibi <sadaf.bibi.dev@gmail.com>
2026-01-16 23:50:46 +0500

    Add Shape and Rectangle classes with area method

--- Day19.py
+++ Day19.py
@@ -1,1 +1,12 @@
+class Shape:
+    def area(self):
+        pass
 
+
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def area(self):
+        return self.length * self.width
